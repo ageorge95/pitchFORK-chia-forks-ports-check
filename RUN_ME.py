94a1f9bd6c5a0f1cfa0d1327205a88fd249f1469
g4m34x <arteni.george.daniel@gmail.com>
2021-10-08 23:35:35 +0300

    added a trick to keep the RUN_ME window alive after execution

--- RUN_ME.py
+++ RUN_ME.py
@@ -1,6 +1,10 @@
 from _pitchfork import pitchfork
+from threading import Thread
+from time import sleep
 
 if __name__ == '__main__':
+
+    Thread(target=sleep, args=[99999]).start() # trick used to keep the window alive
 
     do = pitchfork()
     do.parse_all_cfgs()
