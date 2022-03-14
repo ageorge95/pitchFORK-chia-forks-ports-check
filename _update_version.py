f2128c1f72d8286a8b97539cb4c48cbcde35d655
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-03-14 19:16:18 +0200

    updated the github build actions for windows with a spec file

---
+++ _update_version.py
@@ -0,0 +1,4 @@
+import sys
+
+spec_replacement=str(open("RUN_ME.spec", "r").readlines()).replace(r'name=''\'pitchfork_dev\'''', r'name=''\'pitchfork_{}\''''.format(sys.argv[-1]))
+open("RUN_ME.spec", "w").writelines(eval(spec_replacement))
